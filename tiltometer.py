34630602902ae620c4683c843da3567c6034edbe
djcopley <djrcopley@icloud.com>
2017-06-25 14:00:55 -0700

    Added low-pass filter for pitch/roll data

---
+++ tiltometer.py
@@ -0,0 +1,201 @@
+"""
+5/24/2017
+This program reads data from an accelerometer and a gyroscope, takes the positional
+data and displays information about the vehicles angles.
+MIT LICENCE
+"""
+
+import time
+import gi
+import threading
+import math
+from mpu6050 import mpu6050
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GLib
+
+# MPU static values
+sensor = mpu6050(0x68)
+alpha = 0.5
+start_pitch = None
+start_roll = None
+
+
+def calc_pitch_roll(fXg, fYg, fZg):
+
+    """ Calculates pitch and roll """
+
+    pitch = (math.atan2(fXg, math.sqrt(fYg*fYg + fZg*fZg)) * 180.0) / math.pi
+    roll = (math.atan2(-fYg, fZg) * 180.0) / math.pi
+    
+    return pitch, roll
+
+
+def level_gyro():
+    global start_pitch, start_roll
+    accel = sensor.get_accel_data()
+    start_pitch, start_roll = get_gyro_pos(start_val = True)
+
+
+def get_gyro_pos(**kwargs):
+    
+    """ Returns pitch and roll from mpu """
+
+    accel = sensor.get_accel_data()
+    
+    ax = accel['x']
+    ay = accel['y']
+    az = accel['z']
+
+    fXg = ax * alpha
+    fXg = fXg + (fXg * (1.0 - alpha))
+
+    fYg = ay * alpha
+    fYg = fYg + (fYg * (1.0 - alpha))
+
+    fZg = az * alpha
+    fZg = fZg + (fZg * (1.0 - alpha))
+
+    
+    pitch, roll = calc_pitch_roll(fXg, fYg, fZg)
+
+    if len(kwargs) is 0:
+        return int(pitch - start_pitch), int(roll - start_roll)
+
+    elif kwargs['start_val'] is True:
+        return(pitch, roll)
+
+
+def update_position_gauges():
+    
+    """ Updates the rotation of the Roll and Pitch Gauges """
+    
+    # Object imports
+    pitch_gauge = builder.get_object("pitch_gauge")
+    roll_gauge = builder.get_object("roll_gauge")
+
+    # Wait for main loop
+    while Gtk.main_level() == 0:
+        time.sleep(.01)
+
+    # Update loop
+    while Gtk.main_level() != 0:
+        pitch_val, roll_val = get_gyro_pos()
+        if int(pitch_val) in range(-50, 51):
+            mainloop_do(pitch_gauge.set_from_file, "Assets/pitch-gauge/pitch-gauge%d.png" % int(pitch_val))
+
+        else:
+            mainloop_do(pitch_gauge.set_from_file, "Assets/outside-range/pitch_outside_range_0.png")
+
+        if int(roll_val) in range(-50, 51):
+            mainloop_do(roll_gauge.set_from_file, "Assets/roll-gauge/roll-gauge%d.png" % int(roll_val))
+
+        else:
+            mainloop_do(roll_gauge.set_from_file, "Assets/outside-range/roll_outside_range_0.png")
+
+        time.sleep(.05)
+
+
+def mainloop_do(callback, *args, **kwargs):
+    def cb(_none):
+        callback(*args, **kwargs)
+        return False
+
+    Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT, cb, None)
+
+
+class Handler:
+
+    """ Handler Class - Takes care of events and signals """
+
+    def __init__(self):
+        # Object imports
+        self.preference_window = builder.get_object("pref_window")
+        self.about_dialog = builder.get_object("about_dialog")
+
+        self.pitch_roll_toggle = builder.get_object("pitch_roll_toggle")
+        self.pitch_label = builder.get_object("pitch_label")
+        self.roll_label = builder.get_object("roll_label")
+
+        # Thread variables
+        self.pos_data_update_thread_running = False
+
+        # Start Positional Gyro Data
+        self.pref_pos_data_toggle()
+
+    def pref_pos_data_toggle(self, *args):
+        if not self.pos_data_update_thread_running:
+            threading.Thread(target=self.pos_data_update).start()
+
+    def pos_data_update(self):
+
+        """ Updates positional data labels on screen """
+
+        self.pos_data_update_thread_running = True
+
+        # Wait for gtk main loop
+        while Gtk.main_level() == 0:
+            time.sleep(.01)
+
+        while self.pitch_roll_toggle.get_active() and Gtk.main_level() != 0:
+
+            # Pitch and roll values
+            pitch_val, roll_val = get_gyro_pos()
+
+            pitch_val = "Pitch: " + str(pitch_val)
+            roll_val = "Roll: " + str(roll_val)
+
+            # Update screen
+            mainloop_do(self.pitch_label.set_text, pitch_val)
+            mainloop_do(self.roll_label.set_text, roll_val)
+
+            time.sleep(.05)
+
+        self.pitch_label.set_text("Pitch")
+        self.roll_label.set_text("Roll")
+
+        self.pos_data_update_thread_running = False
+
+    def pref_window_hide(self, *args):
+        self.preference_window.hide()
+
+    @staticmethod
+    def pholder_toggle(*args):
+        
+        """ Place holder method for TBD preference button """
+        
+        print("Test")
+
+    @staticmethod
+    def gyroscope_calibrate(*args):
+        level_gyro()
+
+    def about_dialog_show(self, *args):
+        self.about_dialog.run()
+        self.about_dialog.hide()
+
+    def pref_window_show(self, *args):
+        self.preference_window.show_all()
+
+    def pref_window_close(self, *args):
+        self.preference_window.hide()
+        return True
+
+    @staticmethod
+    def quit_program(self, *args):
+        Gtk.main_quit()
+
+# Builder Imports
+builder = Gtk.Builder()
+builder.add_from_file("tiltometer.glade")
+builder.connect_signals(Handler())
+
+# Main window
+main_window = builder.get_object("main_window")
+main_window.show_all()
+
+# Level Gyro
+level_gyro()
+
+# Main Loop and Threads
+threading.Thread(target=update_position_gauges).start()
+Gtk.main()
